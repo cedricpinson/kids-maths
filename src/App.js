4cee6142076c550ac881d133d620c89621f23c1d
trigrou <trigrou@trigrou.com>
2021-05-19 01:02:18 +0200

    Adds some basics elements

--- src/App.js
+++ src/App.js
@@ -1,102 +1,127 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Paper from '@material-ui/core/Paper';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
+import TextField from '@material-ui/core/TextField';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import DoneIcon from '@material-ui/icons/Done';
+import BackspaceIcon from '@material-ui/icons/Backspace';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    height: '100vh',
+    alignContent: 'center',
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+  },
+
+}));
+
+
+function TopWindow () {
+  const imageWidth = window.screen.width > 414 ? window.screen.width : 414;
+  const imageHeight = Math.round(imageWidth * 9.0 / 16);
+  console.log('width=', imageWidth, 'height=', imageHeight);
+  return (
+    <div 
+        style={{
+          margin: 0,
+          backgroundImage: 'url(sam.jpg)',
+          backgroundSize: "cover",
+          height: imageHeight,
+          width: imageWidth,
+          color: "#f5f5f5"
+        }}
+      />
+  );
+}
+
 
 function Key(props)
 {
     return (
-      <button className="square"
+      <Button fullWidth='True' style={{fontSize: "2.4em"}} 
         onClick={() => props.onClick()}
         >
         {props.value}
-      </button>
+      </Button>
     );
 }
 
 class Keyboard extends React.Component {
 
   renderKey(i) {
-    return <Key 
-             value={i}
-             onClick= {() => this.handleClick(i) }
-             />;
+    return <Key
+    value={i}
+    onClick= {() => this.props.onClick(i) }
+      />;
   }
   
   render() {
-    const status = '';
-    
     return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
+      <Container>
+        <ButtonGroup style={{ justifyContent:'center', display:'flex'}}>
           {this.renderKey(1)}
           {this.renderKey(2)}
           {this.renderKey(3)}
           {this.renderKey(4)}
           {this.renderKey(5)}
-          {this.renderKey('<')}
-        </div>
-        <div className="board-row">
+          </ButtonGroup>
+        <ButtonGroup style={{ justifyContent:'center', display:'flex'}}>
           {this.renderKey(6)}
           {this.renderKey(7)}
           {this.renderKey(8)}
           {this.renderKey(9)}
           {this.renderKey(0)}
-          {this.renderKey('v')}
-        </div>
-        <div className="board-row">
-        </div>
-      </div>
+          </ButtonGroup>
+        <ButtonGroup style={{ justifyContent:'space-around', display:'flex'}}>
+            <IconButton color="primary" onClick={() => this.props.onBackspace()} style={{borderRadius: 0}}>
+              <BackspaceIcon style={{fontSize:'4rem'}}/>
+            </IconButton>
+            <IconButton color="primary" onClick={() => this.props.onValidate()} style={{borderRadius: 0}}>
+              <DoneIcon style={{fontSize:'4rem'}}/>
+            </IconButton>
+          </ButtonGroup>
+</Container>      
     );
   }
 }
 
-
-function Header() {
-  return (
-      <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-      Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-    className="App-link"
-    href="https://reactjs.org"
-    target="_blank"
-    rel="noopener noreferrer"
-      >
-      Learn React
-    </a>
-      </header>
-      
-  )   
-}
-
 function Status(props) {
   return (
-      <div>{props.value}</div>
+      <Paper>{props.value}</Paper>
   )
 }
 
 function Operation(props) {
   return (
-    <div>
-      <h1 className="math-operation">
+    <Container style={{paddingTop: '50px'}}>
+      <Typography variant="h2" component="h2" gutterBottom  style={{textAlign: 'center'}}>
       {props.value.operation}
-      </h1>      
-      <h1 className="math-result">
-      {props.value.result}
-      </h1>
-      <h1 className="math-result">
+      </Typography>      
+      <Typography variant="h2" component="h2" gutterBottom style={{paddingLeft: '10px'}}>
+      = {props.value.result}
+      </Typography>      
+      <h1>
       {props.value.status}
       </h1>
-    </div>
+    </Container>
   )   
 }
 
 function getNumber10() {
     return Math.floor((Math.random() * 10) + 1);
 }
+
 
 class MainPage extends React.Component {
   constructor(props) {
@@ -126,24 +151,18 @@ class MainPage extends React.Component {
     this.setState(this.getNewOperation());
   }
 
-  handleKeyDown(event) {
 
-    if (event.key === "Backspace") {
+  handleBackspace() {
       const newResult = this.state.result.slice(0, this.state.result.length -1);
       this.setState( {...this.state, result: newResult} );
-      console.log(this.state);
-      return;
-    }
-
-    if (event.key in [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]) {
+  }
+  handleNumber(i) {
       let newResult = this.state.result.slice();
-      newResult += event.key;
+      newResult += i;
       this.setState( {...this.state, result: newResult} );
       console.log(this.state);
-      return;
-    }
-
-    if (event.key === "Enter") {
+  }
+  handleValidation() {
       if ( (this.state.operands[0] + this.state.operands[1]) === parseInt(this.state.result)  ) {
         this.setState( {...this.state, status: "true" } );
       } else {
@@ -151,9 +170,35 @@ class MainPage extends React.Component {
       }
 
       setTimeout(() => this.setState(this.getNewOperation()), 1000);
+  }
+  
+  
+  handleKeyDown(event) {
+
+    if (event.key === "Backspace") {
+      this.handleBackspace();
+      return;
+    }
+
+    if (event.key in [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]) {
+      this.handleNumber(event.key);
+      return;
+    }
+
+    if (event.key === "Enter" && this.state.result.length > 0) {
+      this.handleValidation();
       return;
     }
   }
+
+  handleClick(i) {
+    
+    if (i in [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]) {
+      this.handleNumber(i);
+      return;
+    }
+  }
+  
   
   componentDidMount() {
     this.handleKeyFunction = this.handleKeyDown.bind(this);
@@ -165,23 +210,44 @@ class MainPage extends React.Component {
     
   }
   
-  
   render() {
+    
     return (
-    <div className="App">
-        <Header/>
+      <Box >
+        <TopWindow/>
+
         <Status value={this.state.errors}/>
         <Operation value={this.state}/>
-        <Keyboard/>
-    </div>
+
+        <Keyboard
+      value={this.state}
+      onClick={(i) => this.handleClick(i) }
+      onValidate={() => this.handleValidation() }
+      onBackspace={() => this.handleBackspace() }
+        />
+        </Box>
     )
   }
-  
 }
 
 function App() {
+
+  const classes = useStyles();
+
   return (
+    <Container >
+        <Grid container
+      direction="column"
+      justify="center"
+      alignItems="center"
+      className={classes.root}
+        >
+        
     <MainPage/>
+
+        </Grid>
+    </Container>
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
+  <ThemeProvider theme={theme}>
+  {}
+    <CssBaseline />
     <App />
+  </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
